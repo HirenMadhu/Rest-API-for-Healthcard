6d0339a90f998dda22a593f137aca067e0d4d10b
HirenMadhu <hirenmadhu16@yahoo.com>
2020-01-18 12:37:46 +0530

    Setting up basic models

---
+++ src/models/case.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const caseSchema = new mongoose.Schema({
+    HCID : {
+        type: String,
+        required: true
+    },
+    DID: {
+        type: String,
+        required: true
+    },
+    HID : {
+        type: String,
+        required: true
+    },
+    Date_time:{
+        type: Date,
+        required: true
+    },
+    probelm_description:{
+        type: String,
+        required: true
+    },
+    eval_and_cure:{
+        type: String,
+        required: true
+    },
+    viral_disease:{
+        type: String,
+        default: "None"
+    },
+    instructions:[{
+        type: String
+    }],
+    precribed_medicines:[{
+        medicineName:String,
+        dosage: String,
+        extra:String
+    }],
+    nextOPDDate:{
+        type: Date
+    }
+})
+
+const Case = new mongoose.Model('Case', caseSchema)
+
+module.exports = Case
